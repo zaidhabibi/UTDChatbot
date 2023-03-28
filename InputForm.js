ff637b6452639598fc406f34e6f44908fde7da0e
waheedanwar2699 <123407151+waheedanwar2699@users.noreply.github.com>
2023-03-27 20:41:24 -0500

    Add files via upload

--- InputForm.js
+++ InputForm.js
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 
 function InputForm() {
   const [inputValue, setInputValue] = useState('');
+  const [serverMessage, setServerMessage] = useState('');
 
   const handleSubmit = async (e) => {
-    // this done to prevent the page from reloading when a submit button is clicked
-    // which is a default for some reason
     e.preventDefault();
 
     const response = await fetch('http://localhost:5000/chatbot', {
@@ -16,36 +15,25 @@ function InputForm() {
       body: JSON.stringify({ user_input: inputValue }),
     });
 
-    // get the answer and
     const data = await response.json();
-    // print it to the console
-    console.log(data.message);
-
-    //clear text from question box
+    setServerMessage(data.message);
     setInputValue('');
-
-
   }
 
-  // update the input state when it changes
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
-  // it's a form where you can submit an input
   return (
-    <form onSubmit={handleSubmit}
-      style={{
-        position: "fixed",
-        bottom: 50
-      }}
-    >
-      <input type="text" value={inputValue} onChange={handleInputChange}
+    <div>
+      {/* Display the server message */}
+      <p>{serverMessage}</p>
 
-
-      />
-      <button type="submit">Send</button>
-    </form>
+      <form onSubmit={handleSubmit} style={{ position: "fixed", bottom: 50 }}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button type="submit">Send</button>
+      </form>
+    </div>
   );
 }
 
