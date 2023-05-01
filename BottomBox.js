0af711201e93b3f289051dc04d824ba86f367505
waheedanwar2699 <123407151+waheedanwar2699@users.noreply.github.com>
2023-05-01 15:24:35 -0500

    Final changes

--- BottomBox.js
+++ BottomBox.js
@@ -4,7 +4,10 @@ import { View } from 'react-native';
 import React from 'react';
 
 //This file creates the background box for the chat box
-
+/* *NOTE*: After implementing the Chat UI module kit, this file is no longer
+    implemented in App.js. However, it is being kept to provide documentation into
+    our first phases of developing the chatbot.
+*/
 class BottomBox extends React.Component {
     render() {
         return (
